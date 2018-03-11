fb3ddbfd40127e7b99adfdcbb6729a5577f8b9dc
dimshik100 <dimshik100@gmail.com>
2018-03-12 00:41:08 +0200

    Updated probability format. Load data via ajax. Normalized precentage.

--- js/coffee.js
+++ js/coffee.js
@@ -21,146 +21,66 @@ probability
 The array is arranged according to hours in a 24 hour cycle.
 	probability[18] -> 18:00
 */
-var probability = [
-	{
-		time: "00:00",
-		probability: "0",
-	},
-	{
-		time: "01:00",
-		probability: "0",
-	},
-	{
-		time: "02:00",
-		probability: "0.3968253968",
-	},
-	{
-		time: "03:00",
-		probability: "0",
-	},
-	{
-		time: "04:00",
-		probability: "0",
-	},
-	{
-		time: "05:00",
-		probability: "0",
-	},
-	{
-		time: "06:00",
-		probability: "0",
-	},
-	{
-		time: "07:00",
-		probability: "3.174603175",
-	},
-	{
-		time: "08:00",
-		probability: "3.571428571",
-	},
-	{
-		time: "09:00",
-		probability: "4.365079365",
-	},
-	{
-		time: "10:00",
-		probability: "13.88888889",
-	},
-	{
-		time: "11:00",
-		probability: "8.333333333",
-	},
-	{
-		time: "12:00",
-		probability: "4.761904762",
-	},
-	{
-		time: "13:00",
-		probability: "8.73015873",
-	},
-	{
-		time: "14:00",
-		probability: "9.523809524",
-	},
-	{
-		time: "15:00",
-		probability: "7.142857143",
-	},
-	{
-		time: "16:00",
-		probability: "13.88888889",
-	},
-	{
-		time: "17:00",
-		probability: "9.126984127",
-	},
-	{
-		time: "18:00",
-		probability: "4.365079365",
-	},
-	{
-		time: "19:00",
-		probability: "2.380952381",
-	},
-	{
-		time: "20:00",
-		probability: "1.984126984",
-	},
-	{
-		time: "21:00",
-		probability: "1.984126984",
-	},
-	{
-		time: "22:00",
-		probability: "1.587301587",
-	},
-	{
-		time: "23:00",
-		probability: "0.7936507937",
-	}
-];
+var probability;
 
+var maxProbability;
 
 
 /* WEP - Words of estimative probability
 	https://en.wikipedia.org/wiki/Words_of_estimative_probability
 	I twisted the definitions a little bit :)
 */
+
 var wep = [
 	{
-		max: 14,
-		min: 12,
-		text: "Almost Definitely" //2
+		max: 100,
+		min: 93,
+		text: "Almost Definitely"
 	},
 	{
-		max: 12,
-		min: 9,
-		text: "Almost Certain" //2
+		max: 93,
+		min: 75,
+		text: "Almost Certain"
 	},
 	{
-		max: 9,
-		min: 4,
-		text: "Probably" //6
+		max: 75,
+		min: 50,
+		text: "Probably"
 	},
 	{
-		max: 4,
-		min: 2,
-		text: "Likely" //3
+		max: 50,
+		min: 30,
+		text: "Likely"
 	},
 	{
-		max: 2,
-		min: 1,
-		text: "Unlikely" //3
+		max: 30,
+		min: 7,
+		text: "Unlikely"
 	},
 	{
-		max: 1,
+		max: 7,
 		min: 0,
-		text: "Probably Not" //8
+		text: "Probably Not"
 	}
 ];
 
 
-function normalizeProbability(str) {
+function normalizeProbability(probability) {
+
+	var normalizedProbability = probability / maxProbability * 100;
+
+	// fix 100% to look more real, because nothing is certain in life.
+	if (normalizedProbability === 100) {
+		normalizedProbability = 99.42;
+	}
+	console.log(`normalizedProbability (${normalizedProbability}) = real probability (${probability})/ max Probability (${maxProbability}) * 100`);
+
+	return normalizedProbability;
+}
+
+
+function prettyProbability(str) {
+
 	var strArr = str.split('.');
 	var number = strArr[0];
 	var fraction = '';
@@ -183,11 +103,12 @@ function padLeft(str) {
 
 // returns the probability
 function getProbability(hours) {
-	return probability[hours];
+	return probability[hours].probability;
 }
 
 // returns string
 function getWEP(probability) {
+
 	for (var i = 0; i < wep.length; i++) {
 		var curWEP = wep[i];
 		if (probability >= curWEP.min && probability < curWEP.max) {
@@ -198,33 +119,29 @@ function getWEP(probability) {
 
 function timeUpdated(timeString) {
 	var hours = parseInt(timeString.split(":")[0]);
-	var probability = getProbability(hours);
-	var WEP = getWEP(probability);
 
-	console.log(probability, WEP);
+	var normalizedProbability = normalizeProbability(getProbability(hours));
+	var WEP = getWEP(normalizedProbability);
 
-	$('.probability').html(normalizeProbability(probability.toString()));
+	console.log(normalizedProbability, WEP);
+
+	$('.probability').html(prettyProbability(normalizedProbability.toString()));
 	$('.wep').html(WEP);
 
 }
 
-var totalCups;
+$(document).ready(function () {
+	$.getJSON("ajax/hashedTimes_2018-03-11T21:49:14.636Z.json", function (data) {
 
+		$('.total').html(data.totalCups);
+		$('.from-date').html(data.firstDate.split(' ')[0]);
+		$('.to-date').html(data.lastDate.split(' ')[0]);
 
+		probability = data.probabilityArr;
 
+		maxProbability = Math.max.apply(Math, data.probabilityArr.map(function (o) { return o.probability; }));
+		console.log('maxProbability ', maxProbability);
 
-$( document ).ready(function() {
-    $.getJSON("ajax/hashedTimes_2018-03-11T20:35:10.759Z.json", function (data) {
-
-		// calculate total number of cups
-		totalCups = data.reduce((a, b) => a + b, 0);
-	
-		$('.total').html(totalCups);
-	
-		probability = [];
-		for (let i = 0; i < data.length; i++) {
-			probability.push(data[i] / totalCups * 100);
-		}
 		// first init
 		timeUpdated(currentTime);
 	});
